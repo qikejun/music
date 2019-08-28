fac2392c2a7db5f93f1fdc715451ea93d1c6a8d3
peng <1471947564@qq.com>
2019-08-28 09:47:23 +0800

    调整样式:coffee:

--- views/music.js
+++ views/music.js
@@ -15,27 +15,31 @@ var ap = new APlayer({
     {
       name: "发如雪",
       artist: '周杰伦',
-      url: './jay/发如雪_周杰伦_128K.mp3',
-      cover: './jay/十一月的萧邦.jpg',
-      lrc: './jay/发如雪_周杰伦_128K.lrc'
+      url: 'https://yunpenggit.github.io/music/jay/发如雪_周杰伦_128K.mp3',
+      cover: 'https://yunpenggit.github.io/music/jay/十一月的萧邦.jpg',
+      lrc: 'https://yunpenggit.github.io/music/jay/发如雪_周杰伦_128K.lrc',
+      theme: '#ebd0c2'
     }, {
       name: "等你下课",
       artist: '周杰伦',
-      url: './jay/等你下课_周杰伦_128K.mp3',
-      cover: './jay/等你下课.jpg',
-      lrc: './jay/等你下课_周杰伦_128K.lrc'
+      url: 'https://yunpenggit.github.io/music/jay/等你下课_周杰伦_128K.mp3',
+      cover: 'https://yunpenggit.github.io/music/jay/等你下课.jpg',
+      lrc: 'https://yunpenggit.github.io/music/jay/等你下课_周杰伦_128K.lrc',
+      theme: '#46718b'
     }, {
       name: "告白气球",
       artist: '周杰伦',
-      url: './jay/告白气球_周杰伦_128K.mp3',
-      cover: './jay/周杰伦的床边故事.jpg',
-      lrc: './jay/告白气球_周杰伦_128K.lrc'
+      url: 'https://yunpenggit.github.io/music/jay/告白气球_周杰伦_128K.mp3',
+      cover: 'https://yunpenggit.github.io/music/jay/周杰伦的床边故事.jpg',
+      lrc: 'https://yunpenggit.github.io/music/jay/告白气球_周杰伦_128K.lrc',
+      theme: '#ebd0c2'
     }, {
       name: "兰亭序",
       artist: '周杰伦',
-      url: './jay/兰亭序_周杰伦_128K.mp3',
-      cover: './jay/摩杰座.jpg',
-      lrc: './jay/兰亭序_周杰伦_128K.lrc'
+      url: 'https://yunpenggit.github.io/music/jay/兰亭序_周杰伦_128K.mp3',
+      cover: 'https://yunpenggit.github.io/music/jay/摩杰座.jpg',
+      lrc: 'https://yunpenggit.github.io/music/jay/兰亭序_周杰伦_128K.lrc',
+      theme: '#46718b'
     }
   ]
 });
